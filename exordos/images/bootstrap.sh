7f7db4a229bd12f282186f53b56717573caf75a6
Anton Kremenetsky <anton.kremenetsky@gmail.com>
2026-06-05 22:27:56 +0300

    Bump up SDK to 3.0.3, Seed OS to 2.0.1
    Signed-off-by: Anton Kremenetsky <anton.kremenetsky@gmail.com>

--- exordos/images/bootstrap.sh
+++ exordos/images/bootstrap.sh
@@ -73,7 +73,7 @@ if [[ -n "$PERSISTENT_DISK" ]]; then
     migrate_to_persistent "/var/lib/exordos/exordos_core" "${PERSISTENT_MOUNT}/var/lib/exordos/exordos_core"
     mkdir -p /var/lib/exordos/data
     migrate_to_persistent "/var/lib/exordos/data" "${PERSISTENT_MOUNT}/var/lib/exordos/data"
-    migrate_to_persistent_stop_start "/var/lib/postgresql" "${PERSISTENT_MOUNT}/var/lib/postgresql" "postgresql@${PG_VERSION}-main"
+    migrate_to_persistent_stop_start "/var/lib/postgresql" "${PERSISTENT_MOUNT}/var/lib/postgresql" "postgresql@${PG_VERSION}-main" "postgres" "postgres"
     migrate_to_persistent "/etc/exordos_core" "${PERSISTENT_MOUNT}/etc/exordos_core"
     migrate_to_persistent "/home/ubuntu" "${PERSISTENT_MOUNT}/home/ubuntu"
     migrate_to_persistent "/root" "${PERSISTENT_MOUNT}/root"
