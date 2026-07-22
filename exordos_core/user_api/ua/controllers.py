95cd90eaec17b8ea3083290e9983c77d49c13938
vasily <vasily.stepanov@gmail.com>
2026-07-22 16:29:01 +0000

    fix(ua): make AgentController.issue_key race-safe
    It reimplemented the same racy get-or-create pattern already fixed on
    NodeEncryptionKey.get_or_create() (two agents on one node racing to
    issue_key would have the loser crash on the unique node uuid instead
    of getting the winner's key). Delegate to it instead of duplicating.

--- exordos_core/user_api/ua/controllers.py
+++ exordos_core/user_api/ua/controllers.py
@@ -15,13 +15,10 @@
 #    under the License.
 
 from gcl_iam.api import controllers as iam_controllers
-from gcl_sdk.agents.universal.api import crypto as ua_crypto
 from gcl_sdk.agents.universal.dm import models as ua_models
 from restalchemy.api import actions
 from restalchemy.api import controllers as ra_controllers
 from restalchemy.api import resources
-from restalchemy.dm import filters as dm_filters
-from restalchemy.storage import exceptions as ra_storage_exceptions
 
 
 class InternalController(ra_controllers.RoutesListController):
@@ -44,17 +41,7 @@ class AgentController(
     def issue_key(self, resource: ua_models.UniversalAgent):
         self._enforce("issue_key")
 
-        try:
-            enc_key = ua_models.NodeEncryptionKey.objects.get_one(
-                filters={"uuid": dm_filters.EQ(resource.node)}
-            )
-        except ra_storage_exceptions.RecordNotFound:
-            _, key_base64 = ua_crypto.generate_key_base64()
-            enc_key = ua_models.NodeEncryptionKey(
-                uuid=resource.node,
-                private_key=key_base64,
-            )
-            enc_key.insert()
+        enc_key = ua_models.NodeEncryptionKey.get_or_create(resource.node)
 
         return {"key": enc_key.private_key}
 
