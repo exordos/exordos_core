538eec71f4ab47b221b59385f5e5e4bfea1c9de3
slashburygin <slashburygin@gmail.com>
2026-09-10 23:42:48 +0300

    perf(repo): reduce iter_min_period to 1 second

--- exordos_core/repo/builders/repository.py
+++ exordos_core/repo/builders/repository.py
@@ -43,7 +43,7 @@ class RepoProxyBuilderService(
 ):
     def __init__(
         self,
-        iter_min_period: int = 3,
+        iter_min_period: int = 1,
         iter_pause: float = 0.1,
     ) -> None:
         super().__init__(
